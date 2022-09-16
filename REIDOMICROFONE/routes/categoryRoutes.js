a60ff21a2bad61b89e4795110f7ae8f624f1cb08
Romulo Jales <romulojales@outlook.com>
2022-09-16 18:53:38 -0300

    Aplicação do Prettier

--- REIDOMICROFONE/routes/categoryRoutes.js
+++ REIDOMICROFONE/routes/categoryRoutes.js
@@ -1,29 +1,29 @@
-const router = require("express").Router();
-const {Category} = require("../models/Category");
+const router = require('express').Router();
+const { Category } = require('../models/Category');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     Category.find()
-    .exec()
-    .then((data, error) => {
-        if (error) return res.status(400).json({status: false, erros});
+        .exec()
+        .then((data, error) => {
+            if (error) return res.status(400).json({ status: false, erros });
+            return res.status(200).json({
+                status: true,
+                message: 'Get category successfully',
+                data,
+            });
+        });
+});
+
+router.post('/create', (req, res) => {
+    const category = new Category(req, body);
+    category.save((error, data) => {
+        if (error) return res.status(400).json({ status: false, erros });
         return res.status(200).json({
             status: true,
-            message:"Get category successfully",
+            message: 'Get has been category successfully',
             data,
         });
     });
 });
-
-router.post("/create", (req,res) => {
-    const category = new Category(req, body);
-    category.save((error, data) => {
-        if (error) return res.status(400).json({status: false, erros});
-        return res.status(200).json({
-            status: true,
-            message:"Get has been category successfully",
-            data,
-        });
-    })
-})
 
 module.exports = router;
--- REIDOMICROFONE/routes/productRoutes.js
+++ REIDOMICROFONE/routes/productRoutes.js
@@ -1,30 +1,30 @@
-const router = require("express").Router();
-const {Product} = require("../models/Category");
+const router = require('express').Router();
+const { Product } = require('../models/Category');
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
     Product.find()
-        .populate("_category")
+        .populate('_category')
         .exec()
         .then((data, error) => {
-            if (error) return res.status(400).json({status: false, erros});
+            if (error) return res.status(400).json({ status: false, erros });
             return res.status(200).json({
-              status: true,
-              message:"Get Product successfully",
-              data,
+                status: true,
+                message: 'Get Product successfully',
+                data,
             });
         });
 });
 
-router.post("/create", (req,res) => {
+router.post('/create', (req, res) => {
     const product = new Product(req, body);
     product.save((error, data) => {
-        if (error) return res.status(400).json({status: false, erros});
+        if (error) return res.status(400).json({ status: false, erros });
         return res.status(200).json({
             status: true,
-            message:"Product has been category successfully",
+            message: 'Product has been category successfully',
             data,
         });
-    })
-})
+    });
+});
 
 module.exports = router;
